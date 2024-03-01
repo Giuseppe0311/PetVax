369df044335eecd71b85dafe6318b23a4c55daea
angel giuseppe barrera romero <barrerita_romero@hotmail.com>
2024-03-01 17:26:44 -0500

    Se cambio el login y se hizo responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
-    {path:'dashboard', component:DashboardComponent}
+    {path:'dashboard', component:DashboardComponent},
 ];
